8230b1979bc9790f7a9a67ce90f7abaddacb1397
Avi <81462673+Cosmos365@users.noreply.github.com>
2022-12-31 17:56:16 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 
 function preload() {
     song = loadSound("music.mp3");
@@ -10,6 +15,13 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
 }
 
 function draw() {
@@ -18,4 +30,19 @@ function draw() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results) {
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.X;
+        leftWristY = results[0].pose.leftWrist.Y;
+        console.log("leftWristX = "+leftWRistX+" leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.X;
+        rightWristY = results[0].pose.rightWrist.Y;
+        console.log("rightWristX = "+rightWristX+" rightWristY = "+rightWristY);
+    }
 }
